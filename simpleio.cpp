b1913cd8d861d5dcafa330bd607f5eba2aa56bcd
Teodor Markov <mail@teddymarkov.com>
2020-04-05 16:00:59 +0300

    Create simpleio.cpp

---
+++ simpleio.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int favourite_number;
+
+    std::cout << "Enter your favourite number between 1 and 100: " << std::endl;
+    std::cin >> favourite_number;
+    std::cout << "Amazing, " << favourite_number << " is my favorite number too!" << std::endl;
+
+    return 0;
+}
